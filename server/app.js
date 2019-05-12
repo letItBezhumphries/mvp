639d2d5595b51237653e208168b647ee97155493
EricZH <letItBezhumphries>
2019-05-12 02:37:17 -0700

    wip: added react front-end start

--- server/app.js
+++ server/app.js
@@ -6,10 +6,16 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public/dist')))
+app.use(express.static(path.join(__dirname, '../public/dist')));
 
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(cors());
 
 
